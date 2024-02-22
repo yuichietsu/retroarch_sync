7357b2093ee01e564a118f8f909febac0ab727ee
yuichietsu <osoba.udon.men@gmail.com>
2024-02-22 21:11:44 +0900

    updated test

--- test/sync_retroarch.php
+++ test/sync_retroarch.php
@@ -48,7 +48,7 @@ $sync->syncGames(
         'sf_turbo'      => 'full',
         'sms'           => 'full',
         'snes'          => 'full',
-        'tosec_psp'     => 'rand:2g,cso',
+        'tosec_psp'     => 'rand:2g,ext',
         'tosec_snes'    => 'full',
         'ws'            => 'full',
         'wsc'           => 'full',
@@ -60,11 +60,11 @@ $sync->srcPath = '/mnt/d/files/roms/src';
 $sync->dstPath = '/storage/B42F-0FFA/Android/data/com.retroarch/files/ROM/src';
 $sync->syncGames(
     [
-        '3do'    => 'rand:2g,excl(Hatou|Shokutaku|Shin-chan|Kitty|Menkyo|Kanji)',
-        'dc'     => 'rand:2g,chd',
+        '3do'    => 'rand:2g,excl(Hatou|Shokutaku|Shin-chan|Kitty|Menkyo|Kanji|Golf)',
+        'dc'     => 'rand:2g,ext',
         'pcecd'  => 'rand:4g,chd,lock',
         'pcfx'   => 'rand:2g,chd,disks',
-        'psp'    => 'rand:2g,cso',
+        'psp'    => 'rand:2g,ext',
         'psx'    => 'rand:8g,chd,lock,disks',
         'quake'  => 'full',
         'quake2' => 'full:ext',
